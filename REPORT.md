b32d9814bcce8e920ce31490da50f7a4eb4185ce
Ricardo Alvarez <35704560+ralvarezlo@users.noreply.github.com>
2020-10-01 22:30:57 -0600

    Completion of main.c and REPORT.md

--- REPORT.md
+++ REPORT.md
@@ -1,6 +1,13 @@
-##### Name: (Write your name here)
-##### Date: (Write the date here)
+##### Name: Ricardo Alvarez
+##### Date: 10/01/2020
 
 ### CODING CHALLENGE #2: REPORT
 
-(Write your report here)
+This program will read two inputs pertaining to the Systolic and Diastolic pressure readings and will classify it into
+pertaining groups recognized by The American Hear Association (Normal, Elevated, Stage 1 Hypertension, Stage 2
+Hypertension and Hypertensive Crisis).
+
+In order to reduce the if-statements I grouped the simple boolean conditions and grouped them with short circuit 
+evaluations, starting from the higher ranges all the way to the lower in order to avoid multiple redundant checks.
+
+[The American Hear Association Blood Pressure Readings](https://www.heart.org/en/health-topics/high-blood-pressure/understanding-blood-pressure-readings)
--- main.c
+++ main.c
@@ -3,22 +3,25 @@
 *   Filename: main.c
 *   This file measures a person's blood pressure readings and reports their category.
 *   Last modified by: Angel F. Garcia (9/19/2020)
-*   
-*   Check the // TODO comments to see what is missing and needs to be completed.
 */
 
 #include <stdio.h>
 
 int main(void) {
-  int s, d;
+  int systolic, diastolic;
 
   // Read Systolic and Diastolic pressure
-  scanf("%d %d", &s, &d);
+  scanf("%d %d", &systolic, &diastolic);
+  
+  /* The following if statements will classify the blood pressure
+   * readings into the AHA categories.
+   * Due to the gaps in the chart, I'm assuming that the ranges
+   * expressed in the chart as "systolic > x" are inclusive */
+  if (systolic >= 180 || diastolic >= 120) printf("Hypertensive Crisis\n");
+  else if (systolic >= 140 || diastolic >= 90) printf("Stage 2 Hypertension\n");
+  else if (systolic >= 130 || diastolic >= 80) printf("Stage 1 Hypertension\n");
+  else if (systolic >= 120) printf("Elevated\n");
+  else printf("Normal\n");
 
-  // TODO: Complete the rest of the categories
-  if(s < 120) {
-    if(d < 80) {
-      printf("Normal\n");
-    }
-  }
+  return 0;
 }
